b9d565725981713c2e39b78cd924f5a2fbd8b78f
Lyne-7 <lyne.kahihu@strathmore.edu>
2023-07-13 21:47:28 +0300

    Carpool project

--- lib/assistant/assistant_methods.dart
+++ lib/assistant/assistant_methods.dart
@@ -1,6 +1,10 @@
+import 'package:carpool_users/assistant/request_assistant.dart';
 import 'package:carpool_users/global/global.dart';
+import 'package:carpool_users/global/maps_key.dart';
+import 'package:carpool_users/modals/directions.dart';
 import 'package:carpool_users/modals/user_modal.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
 
 class AssistantMethods{
 
@@ -16,5 +20,25 @@ class AssistantMethods{
         userModalCurrentInfo = UserModal.fromSnapshot(snap.snapshot);
     }
     });
+  }
+
+  static Future<String> searchAddressForGeographicCoordinates(Position position, context) async {
+
+    String apiUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey";
+    String humanReadableAddress = '';
+
+    var requestResponse = await RequestAssistant.receiveRequest(apiUrl);
+    if(requestResponse!= 'Error occurred. no response.'){
+      humanReadableAddress = requestResponse['results'][0]["Formatted address"];
+
+      Directions userPickUpAddress = Directions();
+      userPickUpAddress.locationLatitude= position.latitude;
+      userPickUpAddress.locationLongitude= position.longitude;
+      userPickUpAddress.locationName= humanReadableAddress;
+
+     // Provider.of<AppInfo>(context. listen: false).updatePickUpLocationAddress(userPickUpAddress);
+    }
+
+    return humanReadableAddress;
   }
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,17 @@
 import 'dart:async';
 import 'dart:core';
+
+
+
+import 'package:carpool_users/assistant/assistant_methods.dart';
+import 'package:carpool_users/global/maps_key.dart';
+import 'package:geocoder2/geocoder2.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart' as loc;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -19,19 +27,20 @@ class _MainScreenState extends State<MainScreen> {
 
 
   final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController? newGoogleMapController;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+   zoom: 14.4746,
   );
 
   GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
 
   double searchLocationContainerHeight = 220;
-  double waitingResponsefromDriverContainerHeight =0;
+ double waitingResponsefromDriverContainerHeight =0;
   double assignedDriverInfoContainerHeight = 0;
 
-  Position? userCurrentPosition;
+ Position? userCurrentPosition;
   var geoLocation = Geolocator();
 
   LocationPermission? _locationPermission;
@@ -40,31 +49,124 @@ class _MainScreenState extends State<MainScreen> {
   List<LatLng> pLineCoordinatedList = [];
   Set<Polyline> polylineSet= {};
 
-  Set<Marker> markerSet ={};
-  Set<Circle> circleSet= {};
+  Set<Marker> markersSet ={};
+ Set<Circle> circlesSet= {};
 
- // String userName = '';
-  //String userEmail = '';
+  String userName = '';
+  String userEmail = '';
 
-  bool openNavigationDrawer = true;
+ bool openNavigationDrawer = true;
 
   bool activeNearbyDriverKeysLoaded = false;
 
   BitmapDescriptor? activeNearbyIcon;
 
+  locateUserPosition() async {
+    Position cPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    userCurrentPosition = cPosition;
+
+    LatLng latLngPosition = LatLng(userCurrentPosition!.latitude, userCurrentPosition!.longitude);
+    CameraPosition cameraPosition = CameraPosition(target: latLngPosition, zoom: 15);
+
+    newGoogleMapController!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    String humanReadableAddress = await AssistantMethods.searchAddressForGeographicCoordinates(userCurrentPosition!, context);
+    print("This is our address =" + humanReadableAddress);
+  }
+getAddressFromLatLng() async {
+    try{
+      GeoData data = await Geocoder2.getDataFromCoordinates(
+          latitude: pickLocation!.latitude,
+          longitude: pickLocation!.longitude,
+          googleMapApiKey: mapKey
+      );
+      setState(() {
+        _address = data.address;
+      });
+    } catch(e){
+      print(e);
+  }
+}
+  checkIfLocationPermissionAllowed() async {
+    _locationPermission = await Geolocator.requestPermission();
+
+    if(
+    _locationPermission == LocationPermission.denied){
+      _locationPermission = await Geolocator.requestPermission();
+    }
+
+  }
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkIfLocationPermissionAllowed();
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+     onTap: (){
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        body: Stack(
-          children: [
-            GoogleMap(initialCameraPosition: _kGooglePlex
+       body: Stack(
+         children: [
+            GoogleMap(
+                mapType:MapType.normal,
+                myLocationEnabled: true,
+                zoomGesturesEnabled: true,
+                zoomControlsEnabled: true,
+
+                initialCameraPosition: _kGooglePlex,
+              polylines: polylineSet,
+              markers: markersSet,
+              circles: circlesSet,
+              onMapCreated: (GoogleMapController controller){
+                  _controllerGoogleMap.complete(controller);
+                  newGoogleMapController= controller;
+
+                  setState(() {
+
+                  });
+
+                  locateUserPosition();
+              },
+              onCameraMove: (CameraPosition? position){
+                  if(pickLocation != position!.target){
+                    setState(() {
+                      pickLocation = position.target;
+                    });
+                  }
+              },
+              onCameraIdle: (){
+                 getAddressFromLatLng();
+              },
             ),
+           Align(
+             alignment:Alignment.center,
+             child: Padding(
+               padding: const EdgeInsets.only(bottom:35.0),
+               child: Image.asset('images/pick.png',height:45 , width:45 ),
+             ),
+           ),
+           Positioned(
+             top: 40,
+             right: 20,
+             left: 20,
+             child: Container(
+               decoration: BoxDecoration(
+                 border: Border.all(color: Colors.black),
+                 color: Colors.white,
+               ),
+               padding: EdgeInsets.all(20),
+               child: Text(_address ?? 'Set your pickup location',
+                 overflow: TextOverflow.visible, softWrap: true,
+               ),
+
+             ),
+           )
           ],
         )
       )
